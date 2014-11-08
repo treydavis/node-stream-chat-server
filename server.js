2d318deaa88c293b1badc2da6352022ed0890f1b
Trey Davis <trey@pogoseat.com>
2014-11-07 20:06:44 -0800

    Put commands into object

--- server.js
+++ server.js
@@ -11,18 +11,52 @@ var server = net.createServer(function(socket) {
 	console.log('server disconnected');
     });
 
+    var commands = {
+	'say': function(message) {
+	    for (var i=0; i < sockets.length; i++)
+		sockets[i].write(message + '\n');
+	},
+	'rooms': function() {
+	    //Active rooms are:
+	    //<=  * chat (5)
+	    //<=  * hottub (2)
+	    //<= end of list.
+	    socket.write('you called rooms');
+	},
+	'join': function() {
+	    //   user joining
+	    //entering room: chat
+	    //<=  * gc
+	    //<=  * gc_reviewer (** this is you)
+	    //<=  * foo
+	    //<=  * user1
+	    //<=  * user2
+	    //<=  * y2kcrisis
+	    //<= end of list.
+
+	    //   others in room
+	    //* new user joined chat: user7
+	},
+	'leave': function() {
+	    // in room
+	    //* user has left chat: gc_reviewer (** this is you)
+	},
+	'quit': function() {
+	    socket.end('BYE\n');
+	}
+    };
+
     var didRecieveMessage = function(message) {
+	var command = 'say';
 	var commandPattern = new RegExp(/^\/(.*)\s(.*)$/);
 	var commandPatternMatches = message.match(commandPattern);
 	if (commandPatternMatches) {
 	    var command = commandPatternMatches[1];
 	    message = commandPatternMatches[2];
-	    console.log('command: ', command, 'message: ', message);
 	}
-	else {
-	    for (var i=0; i < sockets.length; i++)
-		sockets[i].write(message + '\n');
-	}
+	if (commands[command] == undefined) return;
+	commands[command](message);
+	console.log('command: ', command, 'message: ', message);
     };
 
     var unparsedData = '';
